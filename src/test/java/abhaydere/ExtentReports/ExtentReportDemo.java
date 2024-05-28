9891ba8dfbcb6acfaaabb6e27a1b9cbad07f7b9f
abhay <abhaydere22@gmail.com>
2024-05-28 12:48:41 +0530

    ExtentReports

---
+++ src/test/java/abhaydere/ExtentReports/ExtentReportDemo.java
@@ -0,0 +1,45 @@
+package abhaydere.ExtentReports;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportDemo {
+	ExtentReports extent;
+	
+	@BeforeTest
+	public void config() 
+	{
+		//ExtentReports and ExtetntSparkReporter class
+		String path = System.getProperty("user.dir")+"\\reports\\inedx.html";
+	
+		
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Result");
+		reporter.config().setDocumentTitle("Test Results");
+		
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester","Abhay Dere");		
+	}
+	
+	@Test
+	public void initialDemo()
+	{
+		ExtentTest test = extent.createTest("Initial Demo");
+		System.setProperty("WebDriver.chrome.Driver", "/15dec22_laptop/Selenium/Setup/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+		test.fail("consider this test is failed");		
+		extent.flush();
+	}
+
+	
+}
